12a3b4fd778c31f0fd42743927aa2bc48d7045c5
azoff <jazoff@gmail.com>
2010-04-06 02:41:28 -0700

    added syntax highlighter

---
+++ static/js/overscroll.js
@@ -0,0 +1,33 @@
+$(function(content, source, overlay){
+	
+	SyntaxHighlighter.all();
+	
+	content = $("#content");
+	
+	$("#screen", content).overscroll();
+	
+	source = $("#source", content);
+	
+	overlay = $("#overlay", content);
+	
+	overlay.delayedFadeIn = function() {
+		overlay.fadeTimeout = setTimeout(function(){
+			overlay.fadeTimeout = null;
+			overlay.stop(true, true).fadeIn("fast");
+		}, 1000);
+	};
+	
+	overlay.instantFadeOut = function() {
+		if(overlay.fadeTimeout != null) {
+			clearTimeout(overlay.fadeTimeout);
+			overlay.fadeTimeout = null;
+		} else {
+			overlay.stop(true, true).fadeOut("fast");
+		}
+	};
+	
+	overlay.fadeIn("fast").parent()
+		.bind("mouseenter", overlay.instantFadeOut)
+		.bind("mouseleave", overlay.delayedFadeIn);
+	
+});
